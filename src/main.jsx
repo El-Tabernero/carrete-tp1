2f38c4ff5c1ce212bf975a76a08f25e30578feeb
El-Tabernero <rodri.dojas@gmail.com>
2025-11-21 02:30:59 -0300

    corrección de archivos clave

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { AuthProvider } from './components/screens/AuthContext.tsx';
+import App from './App.jsx';
+import { AuthProvider } from './components/screens/AuthContext.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './index.css'; 
 
